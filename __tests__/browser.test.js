45526c9d60e40cf21323e8991e1d4a539218f114
Matias Infante <minfante@applaudostudios.com>
2023-05-24 15:55:44 -0300

    Add browser config

--- __tests__/browser.test.js
+++ __tests__/browser.test.js
@@ -3,10 +3,19 @@ const puppeteer = require('puppeteer')
 describe('First puppeteer test', () => {
     it('should open and close the browser', async () => {
         const browser = await puppeteer.launch({
-            headless: false
+            headless: false,
+            slowMo: 0,
+            devtools: false,
+            defaultViewport: null,
+            // defaultViewport: {
+            //     width: 1980,
+            //     height: 1080
+            // },
+            // args: ['--window-size=1920,1080']
         })
         const page = await browser.newPage()
         await page.goto('https://www.google.com.ar')
         await browser.close()
-    }, 10000)
+
+    }, 30000)
 });
